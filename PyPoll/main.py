ea1d9dabf97702619ac6d9c2a3fb0aa01080bb34
jimlhaugen <jhaugen@mindspring.com>
2024-02-23 11:39:00 -0800

    Done!  All but the commneting and README.md file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,19 +9,6 @@ import csv
 CandidateName = ['Charles Casper Stockham','Diana DeGette','Raymon Anthony Doane']
 CandidateVoteCount = [0, 0, 0]
 TotalVotes = 0
-TotalProfitLoss = 0
-VoteCountCandidate1 = 0
-VoteCountCandidate2 = 0
-VoteCountCandidate3 = 0
-CandidateName1 = 0
-CandidateName2 = 0
-CandidateName3 = 0
-CarryForwardMonthProfit = 0
-GreatestMonthlyIncreaseMonth = '' 
-GreatestMonthlyDecreaseMonth = '' 
-ChangeEntirePeriod = 0
-Count = 0
-ProfitBeginningPeriod = 0
 
 # Set path for file
 csvpath = os.path.join("Resources", "election_data.csv")
@@ -36,20 +23,9 @@ with open(csvpath) as csvfile:
 
         if row[2] in CandidateName:
             TotalVotes = TotalVotes + 1
-
-            #if candidate == CandidateName[0]:            
-            #    VoteCountCandidate1 = VoteCountCandidate1 + 1 
-            #if candidate == CandidateName[1]:            
-            #    VoteCountCandidate2 = VoteCountCandidate2 + 1
-            #if candidate == CandidateName[2]:
-            #    VoteCountCandidate3 = VoteCountCandidate3 + 1 
             for x in range(3):
                 if candidate == CandidateName[x]:            
-                    CandidateVoteCount[x] += 1 
-           # if candidate == CandidateName[1]:            
-             #   CandidateVoteCount[1] += 1 
-           # if candidate == CandidateName[2]:
-          #      CandidateVoteCount[2] += 1 
+                    CandidateVoteCount[x] += 1
 
         if((CandidateVoteCount[0] > CandidateVoteCount[1]) and (CandidateVoteCount[0] > CandidateVoteCount[2])): 
             Winner = CandidateName[0]
@@ -59,8 +35,6 @@ with open(csvpath) as csvfile:
             Winner = CandidateName[2]
         
 def percent(num1, num2):
-    num1 = float(num1)
-    num2 = float(num2)
     percentage = "{:.3%}".format(num1 / num2)
     return percentage
   
@@ -68,8 +42,10 @@ print("Election Results" + "\n\n\n\n")
 print("-" * 25 + "\n\n\n\n")
 print("Total Votes:", TotalVotes, "\n\n\n\n")
 print("-" * 25 + "\n\n\n\n")
+
 for y in range(3):
     print(CandidateName[y], ": ", percent(CandidateVoteCount[y], TotalVotes), " (", CandidateVoteCount[y], ")", "\n\n\n\n", sep='')
+    
 print("-" * 25 + "\n\n\n\n")
 print("Winner: " + Winner + "\n\n\n\n")
 print("-" * 25)
@@ -80,20 +56,16 @@ print("-" * 25)
 output_path = os.path.join("output.txt")
 
 # Open the file using "write" mode. Specify the variable to hold the contents
-#with open(output_path, 'w') as textfile:
+with open(output_path, 'w') as textfile:
 
-#    textfile.write("Election Results" + "\n\n\n\n")
+    textfile.write("Election Results" + "\n\n\n\n")
+    textfile.write("-" * 25 + "\n\n\n\n")
+    textfile.write("Total Votes:" + str(TotalVotes) + "\n\n\n\n")
+    textfile.write("-" * 25 + "\n\n\n\n")
 
- #   textfile.write("-" * 25 + "\n\n\n\n")
+    for z in range(3):
+        textfile.write(CandidateName[z] + ": " + percent(CandidateVoteCount[z], TotalVotes) + " (" + str(CandidateVoteCount[z]) + ")" + "\n\n\n\n" ) # + sep=''
 
- #   textfile.write("Total Votes: " + str(TotalVotes) + "\n\n\n\n")
-
-  #  textfile.write("-" * 25 + "\n\n\n\n")
-
-   # textfile.write(CandidateName1, ":", + str(VoteCountCandidate1) + "\n\n\n\n")
-
-   # textfile.write("VoteCountCandidate1: " + "\n\n\n\n") python main.py
-
- #   textfile.write("Greatest Increase in Profits: " + str(VoteCountCandidate1) + " ($" + str(VoteCountCandidate1) + ")" + "\n\n\n\n")
-
-  #  textfile.write("Greatest Decrease in Profits: " + str(VoteCountCandidate1) + " ($" + str(VoteCountCandidate1) + ")"
+    textfile.write("-" * 25 + "\n\n\n\n")
+    textfile.write("Winner: " + Winner + "\n\n\n\n")
+    textfile.write("-" * 25)
